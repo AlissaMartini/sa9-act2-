02864786265b355f0b2c994a401c8bd4c4c7c2f0
Alissa Martini <mmrtini4@memphis.edu>
2024-02-24 02:32:06 -0600

    answers

---
+++ lib/bank_account.rb
@@ -0,0 +1,18 @@
+class BankAccount
+    attr_reader :balance
+  
+    def initialize(balance = 0)
+      @balance = balance
+    end
+  
+    def deposit(amount)
+      @balance += amount
+    end
+  
+    def withdraw(amount)
+      if amount <= @balance
+        @balance -= amount
+      end
+    end
+  end
+  
