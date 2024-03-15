45b6b0f706bc89a8854b396d47496145d6031d6f
Alissa Martini <mmrtini4@memphis.edu>
2024-03-15 14:34:54 -0500

    fixed bank spec file

--- bank_account_spec.rb
+++ bank_account_spec.rb
@@ -1,31 +1,32 @@
 require 'bank_account.rb'
 
 RSpec.describe BankAccount do
-  let(:account) { BankAccount.new(1000) }
-
   describe "#deposit" do
     it "increases the balance by the deposit amount" do
-      account.deposit(1000)
-      expect(account.balance).to eq(2000)
+      account = BankAccount.new(1000)
+      account.deposit(500)
+      expect(account.balance).to eq(1500)
     end
   end
 
   describe "#withdraw" do
     it "decreases the balance by the withdrawal amount if funds are available" do
-      account.withdraw(500)
-      expect(account.balance).to eq(500)
+      account = BankAccount.new(1000)
+      account.withdraw(850)
+      expect(account.balance).to eq(150)
     end
 
     it "does not change the balance if insufficient funds" do
-      account.withdraw(1500)
-      expect(account.balance).to eq(500)
+      account = BankAccount.new(150)
+      account.withdraw(200)
+      expect(account.balance).to eq(150)
     end
   end
 
   describe "#balance" do
     it "returns the current balance" do
-      expect(account.balance).to eq(500)
+      account = BankAccount.new(100)
+      expect(account.balance).to eq(100)
     end
   end
 end
-
--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,18 +1,18 @@
 class BankAccount
-    attr_reader :balance
+  attr_reader :balance
   
-    def initialize(balance = 0)
-      @balance = balance
-    end
+  def initialize(balance = 0)
+    @balance = balance
+  end
   
-    def deposit(amount)
-      @balance += amount
-    end
+  def deposit(amount)
+    @balance += amount
+  end
   
-    def withdraw(amount)
-      if amount <= @balance
-        @balance -= amount
-      end
+  def withdraw(amount)
+    if amount <= @balance
+      @balance -= amount
     end
   end
+end
   
